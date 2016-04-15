15fcae6547bf61fadfe3bfa809bf2042c2f1b19a
Michel Mentz Fernandes <michel.fernandes@cwinet.local>
2016-04-15 17:41:24 -0300

    feat(Ofensiva): implementar metodo de atacar com a estrategia 3.

--- src/modulo-01-java-OO/projeto-lotr-bluej/OfensivaIntercala.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/OfensivaIntercala.java
@@ -7,24 +7,46 @@ public class OfensivaIntercala implements Estrategia
     public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
         return ordemDoUltimoAtaque;
     }
-    
     public void atacar(ArrayList<Elfo> exercitoVivo,ArrayList<Dwarf> dwarves){
-        ArrayList<Elfo> ordenado = ordenaOfensiva(exercitoVivo);
-        for(int i=0;i<ordenado.size();i++){
-            for (int j=0; i<dwarves.size();j++){
-                ordenado.get(i).atirarFlecha(dwarves.get(j));
+        if(verificaMeioAMeio(exercitoVivo)){
+            ArrayList<Elfo> ordenado = ordenaOfensivaIntercalada(exercitoVivo);
+            for(int i=0;i<ordenado.size();i++){
+                for (int j=0; j<dwarves.size();j++){
+                    ordenado.get(i).atirarFlecha(dwarves.get(j));
+                }
+                ordemDoUltimoAtaque.add(exercitoVivo.get(i));
             }
-            ordemDoUltimoAtaque.add(exercitoVivo.get(i));
         }
-        
     }
     
-    private ArrayList<Elfo> ordenaOfensiva(ArrayList<Elfo> elfos){
-        ArrayList<Elfo> ordenado = new ArrayList<>();
-        boolean alterna;
-        /////////////////////////////////////////////////////
-        /////////// EM ANDAMENTO/////////////////////////////
-        /////////////////////////////////////////////////////
-        return ordenado;
+    private ArrayList<Elfo> ordenaOfensivaIntercalada(ArrayList<Elfo> elfos){
+        ArrayList<Elfo> intercala = new ArrayList<>();
+        int index0=0;
+        int index1=1;
+        for(int i=0;i<elfos.size();i++){
+            if(elfos.get(i) instanceof ElfoVerde){
+                intercala.add(index0,elfos.get(i));
+                index0+=2;
+            }else{
+                intercala.add(index1,elfos.get(i));
+                index1+=2;
+            }
+        }
+        return intercala;
+    }
+    
+    private boolean verificaMeioAMeio(ArrayList<Elfo> elfos){
+        int contVerde=0;
+        int contNoturno=0;
+        for(int i=0;i<elfos.size();i++){
+            if(elfos.get(i) instanceof ElfoVerde){
+                contVerde++;
+            }else{
+                contNoturno++;
+            }
+        }
+        
+        if (contNoturno != contVerde)return false;
+        else return true;
     }
 }
---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/OfensivaIntercalaTest.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OfensivaIntercalaTest
+{
+    @After
+    public void gc() {
+        System.gc();
+    }
+    @Test
+    public void atacarCom2ElfosNoturnosE2ElfosVerdesIntercalando(){
+        Estrategia estrategia = new OfensivaIntercala();
+        ArrayList<Elfo> exercito = new ArrayList<>();
+        ArrayList<Dwarf> exercitoAnao = getExercitoAnao();
+        Elfo e1 = new ElfoNoturno("E1");
+        Elfo e2 = new ElfoNoturno("E2");
+        Elfo e3 = new ElfoVerde("E3");
+        Elfo e4 = new ElfoVerde("E4");
+        exercito.add(e1);
+        exercito.add(e2);
+        exercito.add(e3);
+        exercito.add(e4);
+        estrategia.atacar(exercito,exercitoAnao);
+        assertEquals(e3,estrategia.getOrdemDoUltimoAtaque().get(0));
+        assertEquals(e1,estrategia.getOrdemDoUltimoAtaque().get(1));
+        assertEquals(e4,estrategia.getOrdemDoUltimoAtaque().get(2));
+        assertEquals(e2,estrategia.getOrdemDoUltimoAtaque().get(3));
+    }
+    
+    private ArrayList<Dwarf> getExercitoAnao(){
+        ArrayList<Dwarf> ex = new ArrayList<Dwarf>();
+        Dwarf d1 = new Dwarf("D1");
+        Dwarf d2 = new Dwarf("D2");
+        ex.add(d1);
+        ex.add(d2);
+        return ex;
+    }
+}
