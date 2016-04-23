d22770df67b0331d7617a59a08da06620279eeb8
michelfernandes <michelmf_rs@hotmail.com>
2016-04-23 17:21:36 -0300

    feat(Exercício3): adicionar exercícios 3, 4, 5, 6 de banco  de dados.

--- src/modulo-02-banco-parte1/SQLQuery4.sql
+++ src/modulo-02-banco-parte1/SQLQuery4.sql
@@ -1,4 +1,4 @@
-select * from associado
+select * from departamento
 
 --1)
 select	a.NomeEmpregado,
@@ -14,9 +14,43 @@ from	associado a
 
 --3)
 select	a.UF,
-		count(1) as Total_Cidades,
-		b.Nome,
+		count(1) as Total_Cidades
 from cidade a
-inner join associado b on a.idcidade = b.idcidade
-group by UF,b.nome
-	--andamento
+where not exists (	select 1
+					from associado b
+					where a.idcidade = b.idcidade
+				 )
+group by UF
+
+--4)
+select	a.Nome,
+		b.UF,
+		case 
+		when b.uf in ('RS','SC','PR') THEN '***' 
+		end RegiaoSul
+from associado a
+	left join cidade b on a.idcidade=b.idcidade;
+
+--5)
+select	a.NomeEmpregado as NomeEmpregado,
+		b.NomeEmpregado as NomeGerente,
+		c.nomedepartamento as DepartamentoEmpregado,
+		d.nomedepartamento as DepartamentoGerente
+from empregado a
+	left join empregado b on a.idgerente = b.idempregado
+	left join departamento c on a.iddepartamento = c.iddepartamento
+	left join departamento d on b.iddepartamento = d.iddepartamento
+
+--6)
+begin transaction
+go
+Select * Into EmpregadoAux From Empregado
+update EmpregadoAux 
+	set salario += salario * 0.145
+where idempregado in (
+	select a.idempregado
+	from empregadoaux a
+	join departamento b on a.iddepartamento = b.iddepartamento
+	where b.localizacao in ('sao paulo') 
+)
+commit
