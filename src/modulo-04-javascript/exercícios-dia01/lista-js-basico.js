767b191609e592bc2fcc22523b5bbb11cffcba1f
Michel Mentz Fernandes <michelmf_rs@hotmail.com>
2016-05-02 17:35:35 -0300

    feat(lista-js-basico.js): adicionar exercício daisyGame.

---
+++ src/modulo-04-javascript/exercícios-dia01/lista-js-basico.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// 1 - Daisy Game!
+var daisyGame = function(number){
+  return number%2 === 0 ? 'Love me not' : 'Love me';
+}
+
+// 2 - Maior texto
+var maiorTexto = function(array){
+
+
+  return maiorTexto;
+}
