06bb227c8d778164d3d6497b3c7bbc6f4ca394d9
michelfernandes <michelmf_rs@hotmail.com>
2016-05-11 02:20:48 -0300

    feat(SobreMimModel): adicionar o Model da página sobre.

---
+++ src/modulo-05-dotnet/Dia03/01 - MVC/MVC/MVC/Models/SobreMimModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class SobreMimModel
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public DateTime DataNascimento;
+        public string Pais { get; set; }
+        public string Cidade { get; set; }
+        public string Gosta { get; set; }
+        public string NaoGosta { get; set; }
+    }
+}
