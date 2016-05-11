78ca79696d7cde0d0696dcc5ae845feee1d6b243
michelfernandes <michelmf_rs@hotmail.com>
2016-05-11 11:10:12 -0300

    feat(css): adicionar arquivos css para o projeto.

---
+++ src/modulo-05-dotnet/Dia03/01 - MVC/MVC/MVC/Controllers/DesafioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class DesafioController : Controller
+    {
+        // GET: Desafio
+        public ActionResult Index(string aluno)
+        {
+
+            if (aluno == null) aluno = "michel";
+
+            ViewBag.CssRecebido = aluno;
+            return View();            
+        }
+    }
+}
