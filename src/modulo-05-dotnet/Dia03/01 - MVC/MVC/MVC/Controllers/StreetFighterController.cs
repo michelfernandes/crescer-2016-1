596c53566588fc1361d5e19f8edb66a806d8afec
michelfernandes <michelmf_rs@hotmail.com>
2016-05-11 02:19:01 -0300

    feat(StreetFighterController): adicionar duas novas actions para as Views do Exercício 1.

--- src/modulo-05-dotnet/Dia03/01 - MVC/MVC/MVC/Controllers/StreetFighterController.cs
+++ src/modulo-05-dotnet/Dia03/01 - MVC/MVC/MVC/Controllers/StreetFighterController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,10 +8,30 @@ using System.Web.Mvc;
 namespace MVC.Controllers
 {
     public class StreetFighterController : Controller
-    {        
+    {
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult FichaTecnica()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            var Pessoa = new SobreMimModel();
+
+            Pessoa.Nome = "Michel Mentz Fernandes";
+            Pessoa.Idade = 19;
+            Pessoa.DataNascimento = new DateTime(1997, 01, 14);
+            Pessoa.Pais = "Brasil";
+            Pessoa.Cidade = "Portão";
+            Pessoa.Gosta = "Longas caminhadas na praia";
+            Pessoa.NaoGosta = "Brócolis";
+
+            return View(Pessoa);
         }
     }
 }
