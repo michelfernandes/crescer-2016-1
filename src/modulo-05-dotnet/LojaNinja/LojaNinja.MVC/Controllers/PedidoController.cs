9975e3eb72fb264f489f59f82ecb0dd428720da3
Michel Mentz Fernandes <michelmf_rs@hotmail.com>
2016-05-12 17:11:38 -0300

    feat(LojaNinja):adicionar arquivos de codigo dos instrutores referentes a loja ninja.

---
+++ src/modulo-05-dotnet/LojaNinja/LojaNinja.MVC/Controllers/PedidoController.cs
@@ -0,0 +1,64 @@
+﻿using LojaNinja.Dominio;
+using LojaNinja.MVC.Models;
+using LojaNinja.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaNinja.MVC.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private RepositorioVendas repositorio = new RepositorioVendas();
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Salvar(PedidoModel model)
+        {
+            if (model.Estado == "RS" && model.Cidade == "SL")
+                ModelState.AddModelError("", "Cidade e Estado inválidos");
+
+            if (model.DataEntrega.AddDays(-7) < DateTime.Today)
+                ModelState.AddModelError("DataEntrega", "Data deve ser maior do que 7 dias");
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //var pedido = new Pedido();
+                    //repositorio.IncluirPedido();
+                } 
+                catch(ArgumentException ex)
+                {
+                    ModelState.AddModelError("", ex.Message);
+                    return View("Cadastro", model);
+                }
+
+                return View("Detalhes", model);
+            }
+            else
+            {
+                return View("Cadastro", model);
+            }
+        }
+
+        public ActionResult Detalhes(int id)
+        {
+            var pedido = repositorio.ObterPedidoPorId(id);
+
+            return View(pedido);
+        }
+
+        public ActionResult Listagem()
+        {
+            var pedidos = repositorio.ObterPedidos();
+
+            return View(pedidos);
+        }
+    }
+}
---
+++ src/modulo-05-dotnet/LojaNinja/LojaNinja.MVC/Models/PedidoModel.cs
@@ -0,0 +1,20 @@
+﻿using LojaNinja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinja.MVC.Models
+{
+    public class PedidoModel
+    {
+        public DateTime DataEntrega { get; set; }        
+        public string NomeProduto { get; set; }
+        public decimal Valor { get; set; }
+        public TipoPagamento TipoPagamento { get; set; }
+        public string NomeCliente { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }      
+    }
+}
