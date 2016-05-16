2b8a7b1196811b45e27997586c4d3c9fc55ec93a
michelfernandes <michelmf_rs@hotmail.com>
2016-05-16 09:51:24 -0300

    feat(LoginViewModel.cs): adicionar model de usuario

---
+++ src/modulo-05-dotnet/LojaNinja/LojaNinja.MVC/Models/LoginViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinja.MVC.Models
+{
+    public class LoginViewModel
+    {
+        // Criamos esta model para seja o modelo do post de dados do login.
+            [Required]
+            [DisplayName("E-mail")]
+            [StringLength(100)]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DisplayName("Nome")]
+            [StringLength(100)]
+            public string Nome { get; set; }
+
+            [Required]
+            [DisplayName("Senha")]
+            [StringLength(100)]
+            public string Senha { get; set; }
+
+            [Required]
+            [DisplayName("Confirmar senha")]
+            [StringLength(100)]
+            public string ConfirmarSenha { get; set; }
+
+    }
+}
