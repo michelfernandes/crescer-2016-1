e3d2efc12db80adee9a18e1cee694787c9e35782
michelfernandes <michelmf_rs@hotmail.com>
2016-05-16 00:06:54 -0300

    test(PedidoTest.cs): adicionar cenários de teste para a urgência de pedidos.

--- src/modulo-05-dotnet/LojaNinja/LojaNinja.Domino.Test/PedidoTest.cs
+++ src/modulo-05-dotnet/LojaNinja/LojaNinja.Domino.Test/PedidoTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LojaNinja.Dominio;
 
 namespace LojaNinja.Domino.Test
 {
@@ -7,9 +8,20 @@ namespace LojaNinja.Domino.Test
     public class PedidoTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FazCadastroComUrgência()
         {
-
+            var dataPedido = DateTime.Now.AddDays(2);
+            // dataPedido;
+            var pedido = new Pedido(dataPedido, "katana", 1200, TipoPagamento.Amex, "samurai", "SL", "RS");
+            Assert.AreEqual(true, pedido.PedidoUrgente);
+        }
+        [TestMethod]
+        public void FazCadastroSemUrgência()
+        {
+            var dataPedido = DateTime.Now.AddDays(7);
+            // dataPedido;
+            var pedido = new Pedido(dataPedido, "katana", 1200, TipoPagamento.Amex, "samurai", "SL", "RS");
+            Assert.AreEqual(false, pedido.PedidoUrgente);
         }
     }
 }
