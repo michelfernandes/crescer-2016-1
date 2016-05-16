20a4c450f073e9b17ac989aedf97ff685f0dda50
Michel Mentz Fernandes <michelmf_rs@hotmail.com>
2016-05-16 20:28:08 -0300

    feat(LoginModel): criar model para tratar o login de usuario.

---
+++ src/modulo-05-dotnet/LojaNinja/LojaNinja.MVC/Models/LoginModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinja.MVC.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [DisplayName("E-mail")]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Senha")]
+        [StringLength(100)]
+        public string Senha { get; set; }
+
+    }
+}
