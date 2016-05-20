8ec90dd4aefc522901add02a6f014998efe406c9
Michel Mentz Fernandes <michelmf_rs@hotmail.com>
2016-05-20 13:20:04 -0300

    fix(BuscaEcletica): arrumar bug que não exibia bandas que não tinham algum array de imagens

--- src/modulo-06-ajax/Dia01/Exercícios/Exercícios/scripts/View/BuscaEcletica.js
+++ src/modulo-06-ajax/Dia01/Exercícios/Exercícios/scripts/View/BuscaEcletica.js
@@ -3,6 +3,9 @@
 var $frmBuscaEcletica = $('#frmBuscaEcletica');
 $frmBuscaEcletica.submit(function (e) {
 
+
+    $('#lista').children().detach();
+
     var idArtista;
     var $artista = encodeURI( $('input[name=nome]').val() );
     
@@ -10,13 +13,15 @@ $frmBuscaEcletica.submit(function (e) {
        .done(function (res) {
            idArtista = res.artists.items[0].id
 
+
            $.ajax({ url: 'https://api.spotify.com/v1/artists/' + idArtista + '/albums?limit=50', type: 'GET' })
                 .done(function (ie) {
-                    ie.items.forEach(function (obj) {
+                    ie.items.forEach(function (obj) {                       
 
-                        console.log(idArtista);
-                        $('#lista').append($('<li>').append($('<img>').attr('src', obj.images[1].url)));
-                        console.log(obj.images[1].url);
+                        if (obj.images.length != 0) {
+                            $('#lista').append($('<li>').append($('<img>').attr('src', obj.images[1].url)));
+                        }
+
                     });
                 });
 
