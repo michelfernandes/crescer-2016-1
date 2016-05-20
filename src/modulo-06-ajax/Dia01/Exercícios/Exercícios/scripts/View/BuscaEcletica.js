5ac57b58c8401aaa006e81e68522cd623b79b626
Michel Mentz Fernandes <michelmf_rs@hotmail.com>
2016-05-20 18:10:13 -0300

    feat(douchbag): adicionar tratamento diferenciado para a pesquisa Justin Bieber

--- src/modulo-06-ajax/Dia01/Exercícios/Exercícios/scripts/View/BuscaEcletica.js
+++ src/modulo-06-ajax/Dia01/Exercícios/Exercícios/scripts/View/BuscaEcletica.js
@@ -11,24 +11,28 @@ $frmBuscaEcletica.submit(function (e) {
     
     $.ajax({ url: 'https://api.spotify.com/v1/search?q=' + $artista + '&type=artist', type: 'GET' })
        .done(function (res) {
-           idArtista = res.artists.items[0].id
 
+           var ehOJustin = res.artists.items[0].id === '1uNFoZAHBGtllmzznpCI3s';
+
+           idArtista = !ehOJustin ? res.artists.items[0].id : ( Math.random() < 0.8 ? 'douchebag' : res.artists.items[0].id );
 
            $.ajax({ url: 'https://api.spotify.com/v1/artists/' + idArtista + '/albums?limit=50', type: 'GET' })
-                .done(function (ie) {
-                    ie.items.forEach(function (obj) {                       
+                .then(function onSucess(ie) {
 
+                    ie.items.forEach(function (obj) {
                         if (obj.images.length != 0) {
                             $('#lista').append($('<li>').append($('<img>').attr('src', obj.images[1].url)));
                         }
-
                     });
-                });
+                },
+                function onError(ie) {
+                    $('#lista').append($('<span>').attr('style', 'color:red').text(
+                        'Caro usuário, devido a um erro ' + ie.status + ', não foi possível pesquisar por ' + $('input[name=nome]').val()
+                        ));
+                }
+                );
 
-       });
-
-
-    
+       });   
 
     return e.preventDefault();
 });
