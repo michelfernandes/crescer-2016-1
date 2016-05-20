e8606fa8bfdae0ecd8892b1d4d16a3dbb8ad53c0
michelfernandes <michelmf_rs@hotmail.com>
2016-05-20 09:10:42 -0300

    feat(Exercício1): adicionar arquivos referente exercício 1

---
+++ src/modulo-06-ajax/Dia01/Exercícios/Exercícios/Controllers/SpotifyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercícios.Controllers
+{
+    public class SpotifyController : Controller
+    {
+        // GET: Spotify
+        public ActionResult IronMaiden()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/modulo-06-ajax/Dia01/Exercícios/Exercícios/scripts/View/IronMaiden.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+
+$('#btnCriar').click(function () {
+    $.ajax({ url: 'https://api.spotify.com/v1/artists/6mdiAmATAx73kdxrNrnlao/albums?limit=50', type: 'GET' })
+        .done(function (res) {
+            res.items.forEach(function (obj) {
+                $('#lista').append($('<li>').append($('<img>').attr('src', obj.images[1].url)));
+                console.log(obj.images[1].url);
+            });
+        });
+});
