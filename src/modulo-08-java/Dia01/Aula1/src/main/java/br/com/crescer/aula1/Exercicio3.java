b11888596d818640e45c48ce197cfffa7b8e3aba
Michel Mentz Fernandes <michelmf_rs@hotmail.com>
2016-06-07 18:47:30 -0300

    feat(Aula1): adicionar exercício que retorna dia de semana de acordo com a data informada.

--- src/modulo-08-java/Dia01/Aula1/src/main/java/br/com/crescer/aula1/Exercicio3.java
+++ src/modulo-08-java/Dia01/Aula1/src/main/java/br/com/crescer/aula1/Exercicio3.java
@@ -5,19 +5,38 @@
  */
 package br.com.crescer.aula1;
 
+import java.util.Scanner;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 /**
  *
  * @author michel.fernandes
  */
 public class Exercicio3 {
+       
     
-    public static void main(String[] args) {
+    public static void main(String[] args) {     
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date date = new Date();
-        System.out.println(dateFormat.format(date));
-    }
-    
+        System.out.println("Data e horario atual: " + dateFormat.format(date));
+        
+        
+        try (final Scanner scanner = new Scanner(System.in)) {
+            Calendar c = Calendar.getInstance();
+            String formato = "dd/MM/yyyy";
+            String a;
+            Date data = new Date();
+            System.out.println("Informe uma data: ");
+            a = scanner.nextLine(); 
+            data= new SimpleDateFormat(formato).parse(a);
+            c.setTime(data);
+            int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+            System.out.println(dayOfWeek);
+            
+        } catch (Exception e) {           
+            System.out.println("Data Inválida");
+        }        
+    }    
 }
