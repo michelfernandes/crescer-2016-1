fafa194b7303977cc694eb5a1c52d43cdf138614
Michel Mentz Fernandes <michelmf_rs@hotmail.com>
2016-06-09 18:10:43 -0300

    feat(IPessoa): adicionar interface para implementaçoes dos métodos.

---
+++ src/modulo-08-java/Dia03/aula3/src/main/java/br/com/crescer/aula3/IPessoa.java
@@ -0,0 +1,13 @@
+package br.com.crescer.aula3;
+
+import java.util.List;
+
+public interface IPessoa {    
+    void insert(Pessoa pessoa);
+    void update(Pessoa pessoa);
+    void delete(Pessoa pessoa);
+    List<Pessoa> listAll();
+    List<Pessoa> findNome(String nome);    
+}
+
+
