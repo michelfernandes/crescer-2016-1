57dc9bf1d7e881c719d8994aa4a580ac1470f55f
michelfernandes <michelmf_rs@hotmail.com>
2016-06-19 13:22:45 -0300

    refactor(PessoaService): implementar os métodos find save e delete na classe de serviço de pessoa

--- src/modulo-08-java/Dia08/aula8/spring-crescer-web/src/main/java/br/com/crescer/services/PessoaService.java
+++ src/modulo-08-java/Dia08/aula8/spring-crescer-web/src/main/java/br/com/crescer/services/PessoaService.java
@@ -1,21 +1,38 @@
 package br.com.crescer.services;
 
-import br.com.crescer.Pessoa;
+import br.com.crescer.entity.Pessoa;
+import br.com.crescer.repository.PessoaRepository;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class PessoaService {
-
+    
+    @Autowired
+    PessoaRepository repository;
+    
     public List<Pessoa> list() {
         Pessoa p = new Pessoa();
         p.setNome("service");
         p.setNascimento(new Date());
         return Stream.of(p).collect(Collectors.toList());
+    }
+
+    public Iterable<Pessoa> findAll() {
+        return repository.findAll();
+    }
+
+    public Pessoa save(Pessoa p) {
+        return repository.save(p);
+    }
+
+    public void delete(Long id) {
+        repository.delete(id);
     }
 
 }
